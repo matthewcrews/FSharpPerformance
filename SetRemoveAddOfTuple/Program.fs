ef5109058ee7e986e6ad9a75bc60946ecf1a5f67
Matthew Crews <matthew@crews.email>
2022-05-04 12:29:44 -0700

    added small set

--- SetRemoveAddOfTuple/Program.fs
+++ SetRemoveAddOfTuple/Program.fs
@@ -86,11 +86,11 @@ type BitArrayTracker private (jobCount: int, machineCount: int, values: BitArray
 type Benchmarks () =
     
     let rng = Random 123
-    let jobCount = 1_000
-    let machineCount = 100
+    let jobCount = 100
+    let machineCount = 10
     let valueCount = 100
-    let addValueCount = 10
-    let removeValueCount = 10
+    let addValueCount = 2
+    let removeValueCount = 2
         
     let tupleValues =
         [for _ in 0 .. valueCount - 1 -> 
