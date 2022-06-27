9d689a4ec66282248c2ac0a9f67d8c954729062e
Matthew Crews <matthew@crews.email>
2022-06-27 14:37:13 -0700

    fix

--- TopologicalSort/Version7.fs
+++ TopologicalSort/Version7.fs
@@ -159,8 +159,8 @@ module Graph =
     
     let private createSourcesAndTargets (nodeCount: int) (edges: Edge[]) =
         let nodeCount = LanguagePrimitives.Int32WithMeasure<Units.Node> nodeCount
-        let mutable sourcesAcc = Row.create nodeCount []
-        let mutable targetsAcc = Row.create nodeCount []
+        let sourcesAcc = Row.create nodeCount []
+        let targetsAcc = Row.create nodeCount []
         
         for edge in edges do
             let source = Edge.getSource edge
